78575719aaddfe782bf27390e84d14d2b8358d68
mors4incarnatum <sean_arambulo@dlsu.edu.ph>
2024-11-12 03:21:05 +0800

    Rovick Comments

--- DLSU_SRS.java
+++ DLSU_SRS.java
@@ -1,7 +1,16 @@
 public class DLSU_SRS {
-
+//database
     public static void main(String args[]){
-        
+        // promt log or Reg
+
+        // if log User user= array[2]
+        // controler(user)
+
+        //if reg ask for name, id, etc
+        //User user = new user(name, id, etc)
+        //controller (user)
+        //conttoller.register();
+
     }
 
 }
