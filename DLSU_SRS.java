b198a8d9fb7a90c8fa7c67b5ecb3dc2e6982ac45
Rovick Dompor <rovick_dompor@dlsu.edu.ph>
2024-11-12 03:25:01 +0800

    Update DLSU_SRS.java

--- DLSU_SRS.java
+++ DLSU_SRS.java
@@ -1,7 +1,17 @@
 public class DLSU_SRS {
 
     public static void main(String args[]){
+<<<<<<< Updated upstream
         //rovs
+=======
+        // promt log or Reg
+
+        // if log User user= array[2]
+        // controler(user)
+
+        
+
+>>>>>>> Stashed changes
     }
 
 }
