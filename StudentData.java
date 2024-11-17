001ca356a2ddaf01ea49db5760c93255e709feab
mors4incarnatum <sean_arambulo@dlsu.edu.ph>
2024-11-17 17:40:33 +0800

    FIXED MODELS

--- StudentData.java
+++ StudentData.java
@@ -5,12 +5,40 @@ public class StudentData {
     protected String EAF;
     protected int classDays;
 
+    public StudentData(int studentDataID){
+        this.studentDataID = studentDataID;
+    }
+
+    public void setTrimesterID(int trimesterID){
+        this.trimesterID = trimesterID;
+    }
+
+    public void setIdNumber(int idNumber){
+        this.idNumber = idNumber;
+    }
+
     public void setEAF (String EAF) {
         this.EAF = EAF;
     }
 
+    public int getTrimesterID(){
+        return this.trimesterID;
+    }
+
+    public int getIdNumber(){
+        return this.idNumber;
+    }
+
     public String getEAF(){
         return this.EAF;
+    }
+
+    public void setClassDays(int classDays){
+        this.classDays = classDays;
+    }
+
+    public int getClassDays(){
+        return this.classDays;
     }
     
 }
