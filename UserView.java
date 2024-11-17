8056c96928d863ccffede703affd51aa809766c2
mors4incarnatum <sean_arambulo@dlsu.edu.ph>
2024-11-17 15:59:34 +0800

    ADDED BLANK SRSFRAMES

--- UserView.java
+++ UserView.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 
 public class UserView {
     
-    public void SRSFRAME1(){
+    public void SRSFRAME1_LOGIN(){
         // Create the frame
         JFrame frame = new JFrame("DLSU Shuttle Reservation");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -67,4 +67,30 @@ public class UserView {
         frame.setVisible(true);
 
     }
+
+    public void SRSFRAME2_REGISTRATION(){
+        //TODO: Implement this method
+    }
+
+    public void SRSFRAME3_USERINTERFACE(){
+        //TODO: Implement this method
+    }
+
+    public void SRSFRAME4_ADDSHUTTLEBOOKING(){
+        //TODO: Implement this method
+    }
+
+    public void SRSFRAME5_VIEWSHUTTLEBOOKING(){
+        // TODO: Implement this method
+    }
+
+    public void SRSFRAME6_EDITSHUTTLEBOOKING(){
+        // TODO: Implement this method
+    }
+
+    public void SRSFRAME7_EDITUSERDATA(){
+        // TODO: Implement this method
+    }
+
 }
+
